915565b1020813158a001c3a207c8ff7cfb2fb64
Thedark1337 <thedark1337@thedark1337.com>
2016-10-21 08:52:33 -0500

    Added goodbye page & room settings page

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,34 @@
 
 /* Route Dependencies */
 
-const bodyParser = require('koa-body-parser');
+const bodyParser = require('koa-bodyparser');
 const router = require('koa-router')();
 
 /* Route Files */
+/**
+ * Disabled Routes Due to RIP Plug.dj.
+ * Files:
+ * const alpha = require('./alpha');
+ * const donations = require('./donations');
+ * const index = require('./index/');
+ * Routes:
+ * router.get('/alpha', alpha);
+ * router.get('/donations', donations);
+ */
+
 const about = require('./about');
-const alpha = require('./alpha');
 const ambassadors = require('./ambassadors');
+const alpha = require('./alpha');
 const commands = require('./commands');
 const donations = require('./donations');
 const features = require('./features');
+const goodbye = require('./goodbye');
 const index = require('./index/');
 const install = require('./install');
 const lang = require('./lang');
 const license = require('./license');
 const readme = require('./readme');
+const roomsettings = require('./roomsettings');
 const translate = require('./translate');
 
 /* Routes */
@@ -27,10 +40,12 @@ router.get('/ambassadors', ambassadors);
 router.get('/commands', commands);
 router.get('/donations', donations);
 router.get('/features', features);
+router.get('/goodbye', goodbye);
 router.get('/install', install);
 router.post('/lang', bodyParser(), lang);
 router.get('/license', license);
 router.get('/readme', readme);
+router.get('/roomsettings', roomsettings);
 router.get('/translate', translate);
 
 module.exports = router;
--- routes/lang/index.js
+++ routes/lang/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = function *(next) {
-    const lang = this.request.body && this.request.body.lang || 'en';
+    const lang = (this.request.body && typeof this.request.body.lang === 'string' ? this.request.body.lang : 'en');
+
     this.cookies.set('locale', lang);
     this.status = 200;
+    yield next;
 };
